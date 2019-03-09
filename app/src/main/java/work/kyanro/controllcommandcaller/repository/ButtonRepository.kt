11974baf1c329600d4db7e491a1b1be90d689379
kyanro <kyanro@users.noreply.github.com>
2019-03-09 10:12:48 +0900

    block にしてみた

--- app/src/main/java/work/kyanro/controllcommandcaller/repository/ButtonRepository.kt
+++ app/src/main/java/work/kyanro/controllcommandcaller/repository/ButtonRepository.kt
@@ -7,24 +7,25 @@ import work.kyanro.controllcommandcaller.network.Button
 import work.kyanro.controllcommandcaller.network.CccApiService
 import work.kyanro.controllcommandcaller.network.Dpad
 
-class ButtonRepository(val apiClient: CccApiService) {
+class ButtonRepository(private val apiClient: CccApiService) {
     var waiting = false
     var waitingDfd: Deferred<Unit>? = null
 
-    suspend fun hold(button: Button) {
-        if (waiting) return
+    private suspend fun exec(block: () -> Deferred<Unit>) {
         waiting = true
-        waitingDfd = withContext(Dispatchers.IO) { apiClient.hold(button) }
+        waitingDfd = withContext(Dispatchers.IO) { block.invoke() }
         waitingDfd?.await()
         waiting = false
     }
 
+    suspend fun hold(button: Button) {
+        if (waiting) return
+        exec { apiClient.hold(button) }
+    }
+
     suspend fun release(button: Button) {
         if (waiting) return
-        waiting = true
-        waitingDfd = withContext(Dispatchers.IO) { apiClient.release(button) }
-        waitingDfd?.await()
-        waiting = false
+        exec { apiClient.release(button) }
     }
 }
 
@@ -32,19 +33,20 @@ class DpadRepository(val apiClient: CccApiService) {
     var waiting = false
     var waitingDfd: Deferred<Unit>? = null
 
-    suspend fun hold(button: Dpad) {
-        if (waiting) return
+    private suspend fun exec(block: () -> Deferred<Unit>) {
         waiting = true
-        waitingDfd = withContext(Dispatchers.IO) { apiClient.hold(button) }
+        waitingDfd = withContext(Dispatchers.IO) { block.invoke() }
         waitingDfd?.await()
         waiting = false
     }
 
+    suspend fun hold(button: Dpad) {
+        if (waiting) return
+        exec { apiClient.hold(button) }
+    }
+
     suspend fun release(button: Button) {
         if (waiting) return
-        waiting = true
-        waitingDfd = withContext(Dispatchers.IO) { apiClient.release(button) }
-        waitingDfd?.await()
-        waiting = false
+        exec { apiClient.release(button) }
     }
 }
